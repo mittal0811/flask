662f2302c70c1d7e5e9e4d1faad835b85980d926
--global <--global>
2022-07-18 20:39:29 -0400

    added packages

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+
+# Databse configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flaskreact'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+ma = Marshmallow(app)
 
 
 @app.route('/')
 def index():
-    return jsonify({"hello":"Test version"})
+    return jsonify({"hello": "Test version ssss"})
 
 
 if __name__ == '__main__':
